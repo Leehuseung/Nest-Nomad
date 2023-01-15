31ad618429c4f6753275b6148be9d3c8c2f8a25d
Leehuseung <gntmd4261@naver.com>
2023-01-16 01:28:23 +0900

    2.4 DTOs and Validation part Two

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true, //parameter가 항상 string으로 오지만 number로 받을 수 있게 한다.
+  }));
   await app.listen(3000);
 }
 bootstrap();
