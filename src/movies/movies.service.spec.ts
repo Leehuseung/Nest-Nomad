cf2a2eede5a9f93b42fe4a89bc97da01a652290c
Leehuseung <gntmd4261@naver.com>
2023-01-18 19:02:21 +0900

    3.2 Testing get All and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { defaultMetadataStorage } from "class-transformer/types/storage";
+import { NotFoundException } from "@nestjs/common";
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -16,9 +18,32 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 4', () => {
-    expect(2+2).toEqual(4);
-  })
+  describe('getAll', () => {
+    it('should return array',() => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
 
+  describe('getOne', () => {
 
+    it('should return a movie', () => {
+      service.create({
+        title:'Test Movie',
+        genres: ['test'],
+        year: 2000
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try{
+        service.getOne(999);
+      }catch (e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    })
+  });
 });
