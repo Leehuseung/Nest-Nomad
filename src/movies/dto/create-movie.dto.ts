59107bf4e55460fd024a487fdb606b29b381981e
Leehuseung <gntmd4261@naver.com>
2023-01-19 00:24:13 +0900

    4.1 Testing GET movies id

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -2,12 +2,14 @@ import { IS_NUMBER, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateMovieDto {
   @IsString()
+  @IsOptional()
   readonly title: string;
 
   @IsNumber()
+  @IsOptional()
   readonly year: number;
 
   @IsOptional()
-  @IsString({ each:false})
+  @IsString({ each:true})
   readonly genres: string[];
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,6 +12,14 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    //이렇게 요청하면 실제 서버와 다르게 1이 string타입으로 컨트롤러에 전달된다.
+    //main.ts에서 {transform: true}라는 옵션을 사용했다.
+    //main 함수와 다르게 app을 app = moduleFixture.createNestApplication(); 이렇게 생성하고있음. 해당 생성부분을 똑같이 넣어주자.
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
     await app.init();
   });
 
@@ -29,20 +37,31 @@ describe('AppController (e2e)', () => {
         .expect(200)
         .expect([]);
     });
-    it('POST',() => {
-      return request(app.getHttpServer()).post('/movies')
+    it( 'POST',() => {
+      return request(app.getHttpServer())
+        .post('/movies')
         .send({
-          title : '',
-          year : 2000,
-          genres : ['test']
-        }).expect(201);
+          title:'Test Movie',
+          genres: ['test'],
+          year: 2000
+        })
+        .expect(201);
     })
     it('DELETE',() => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     })
   });
 
-
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
+  });
 
 
 });
